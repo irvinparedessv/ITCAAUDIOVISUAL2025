ec5b9abd802d1f9ef63dd7b3d49dcd01fac6899b
LeonardoMdez <leonardomendez1516@gmail.com>
2025-04-28 23:36:26 -0600

    validacion de login a medias

---
+++ app/layouts/protected-layout.tsx
@@ -0,0 +1,19 @@
+import { useMatches } from "react-router-dom";
+import { routeRoles } from "../types/meta"; // o "@/routes/meta" si lo moviste
+import App from "app/root";
+
+export function ProtectedLayout() {
+  const matches = useMatches();
+  const currentRoute = matches[matches.length - 1];
+  
+  const routeId = currentRoute.id ?? ""; // OJO, no es currentRoute.route.id
+  const allowedRoles = routeRoles[routeId] || [];
+
+  const userRole = "Prestamista"; // luego lo cambias a tu auth real
+
+  if (allowedRoles.length > 0 && !allowedRoles.includes(userRole)) {
+    return <div>Acceso denegado</div>;
+  }
+
+  return <App />;
+}

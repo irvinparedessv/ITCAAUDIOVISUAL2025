d1248a39757378602b62e1dc16fbe51ff57e1353
SaulRivera99 <saulrivera9999@gmail.com>
2025-05-04 18:05:13 -0600

    importando toaster a reserva de equipo

--- app/routes/reservation.tsx
+++ app/routes/reservation.tsx
@@ -1,9 +1,11 @@
 import ProtectedRoute from "~/helpers/ProtectedRoute";
 import EquipmentReservationForm from "../components/EquipmentReservationForm";
+import { Toaster } from "react-hot-toast";
 
 export default function ReservationPage() {
    return (
       <ProtectedRoute>
+        <Toaster position="top-right" />
         <EquipmentReservationForm />
       </ProtectedRoute>
     );

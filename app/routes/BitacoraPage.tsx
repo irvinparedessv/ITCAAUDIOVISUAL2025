09b40ab8ff22a105ef9c3433e79f3f2154b0ba6d
LeonardoMdez <leonardomendez1516@gmail.com>
2025-05-21 15:03:37 -0600

    vista bitacora

---
+++ app/routes/BitacoraPage.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import api from '~/api/axios'
+
+interface Bitacora {
+  id: number;
+  created_at: string;
+  nombre_usuario: string;
+  modulo: string;
+  accion: string;
+  descripcion: string;
+}
+
+export default function BitacoraPage() {
+  const [registros, setRegistros] = useState<Bitacora[]>([]);
+
+  useEffect(() => {
+    api.get('/bitacora').then(res => {
+      setRegistros(res.data.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Bitácora del Sistema</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Usuario</th>
+            <th>Módulo</th>
+            <th>Acción</th>
+            <th>Descripción</th>
+          </tr>
+        </thead>
+        <tbody>
+          {registros.map((log) => (
+            <tr key={log.id}>
+              <td>{new Date(log.created_at).toLocaleString()}</td>
+              <td>{log.nombre_usuario}</td>
+              <td>{log.modulo}</td>
+              <td>{log.accion}</td>
+              <td>{log.descripcion}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}

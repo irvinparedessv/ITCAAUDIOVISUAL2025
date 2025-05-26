599c600a2b7b37f41d3e81149504a5470c6e8807
Josué Reyes <jsalvador.reyes@itca.edu.sv>
2025-05-26 01:12:54 -0600

    Se creo el modal ReservacionEstadoAula para poder notificaciones y correo y cambiar estado de reserva, se agrego la ruta en routes.ts

---
+++ app/routes/ReservacionEstadoAula.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import ReservacionEstadoAula from "~/components/ReservacionEstadoAula";
+
+export default function ReservationStatusAulaFormPage() {
+  const { id } = useParams();
+
+  return (
+    <ReservacionEstadoAula
+      reservationId={Number(id)}
+      currentStatus="" // Puedes ajustar si necesitas estado actual
+    />
+  );
+}
